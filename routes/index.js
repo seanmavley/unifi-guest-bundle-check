5318687cfa15de362c4dda100068d01fc13f30fc
Rexford Nkansah <seanmavley@gmail.com>
2017-05-23 16:51:22 +0000

    Because the process of checking voucher is expensive, the use of Recaptcha is employed to ensure at least humans try to check, to prevent bots from misusing the feature. Still working on displaying the result properly to match understandable units aside the in bytes thing

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,61 @@
 var express = require('express');
 var unifi = require('node-unifiapi');
 var router = express.Router();
+var recaptcha = require('express-recaptcha');
+
 var credentials = require('../credentials');
 
 // Check the credentials_sample
-var u = unifi(credentials);
+var u = unifi({
+  baseUrl: credentials.baseUrl, // The URL of the Unifi Controller
+  username: credentials.username,
+  password: credentials.password,
+});
+
+recaptcha.init(credentials.sitekey, credentials.sitesecret);
 
 /*
   Using Native Express with Twig
 */
-router.get('/native', function(req, res, next) {
-  res.render('index', { title: 'My Title' })
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    captcha: recaptcha.render()
+  })
 });
 
-router.post('/native', function(req, res, next) {
-  u.list_guests()
-    .then((data) => {
-      // remove the hyphen if any
-      let voucher = req.body.voucher.replace(/-/g, "");
-      // console.log(data.data.find(x => x.voucher_code === req.body.voucher));
-      let result = data.data.find(x => x.voucher_code === voucher);
-      
-      res.render('index', { 
-        'voucher_code': req.body.voucher_code,
-        'result': result
-      })
+router.post('/', function(req, res, next) {
+  recaptcha.verify(req, function(error) {
+    if (!error) {
+      u.list_guests()
+        .then((data) => {
+          // remove the hyphen if any
+          let voucher = req.body.voucher_code.replace(/-/g, "");
+          // console.log(data.data.find(x => x.voucher_code === req.body.voucher));
+          let result = data.data.find(x => x.voucher_code === voucher);
 
-    })
-    .catch((err) => {
-      console.log('Error', err);
-      res.render('index', { 'error': err });
-    })
+          res.render('index', {
+            'voucher_code': req.body.voucher_code,
+            'result': result
+          })
+
+        })
+        .catch((err) => {
+          console.log('Error', err);
+          res.render('index', { 'error': err });
+        })
+    } else {
+      res.render('index', { 'error': error })
+    }
+  })
+
 })
 
 /*
   The API version, if using Angular or React
   in the Frontend
 */
-router.get('/', function(req, res, next) {
-  res.json({ 
+router.get('/api', function(req, res, next) {
+  res.json({
     message: 'Welcome to AlwaysOn Bundle Check Page.',
     help: 'To check your AlwaysOn Data Bundle stats, send an API POST method to "/",\
     including the parameter, "voucher_code"\
@@ -46,7 +63,7 @@ router.get('/', function(req, res, next) {
   })
 });
 
-router.post('/', function(req, res, next) {
+router.post('/api', function(req, res, next) {
   u.list_guests()
     .then((data) => {
       // remove the hyphen if any
