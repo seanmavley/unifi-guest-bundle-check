768868d434823800c40cf37347d844f635d94782
Rexford Nkansah <seanmavley@gmail.com>
2017-05-24 19:58:45 +0000

    Refactored the index.js to put the voucher data used/left calculations into separate function Tweaked the checkvoucher.twig page to reflect changes Added a few changes to the API endpoint

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,23 @@ router.get('/check-voucher', function(req, res, next) {
   })
 });
 
+function calculate_usage(result) {
+  /*
+    Calculates the amount of data used and left as per voucher.
+    
+    Convert all values to Megabyte
+    1 Megabyte = 1000000 (a million) bytes
+  */
+  var total_used = (result.rx_bytes + result.tx_bytes) / 1000000
+  var total_left = result.qos_usage_quota - total_used;
+
+  return {
+    'quota': result.qos_usage_quota,
+    'total_used': total_used,
+    'total_left': total_left
+  }
+}
+
 router.post('/check-voucher', function(req, res, next) {
   recaptcha.verify(req, function(error) {
     if (!error) {
@@ -46,18 +63,28 @@ router.post('/check-voucher', function(req, res, next) {
           // console.log(data.data.find(x => x.voucher_code === req.body.voucher));
           var result = data.data.find(x => x.voucher_code === voucher);
 
+          var data = calculate_usage(result);
+
           res.render('checkvoucher', {
+            'result': true,
             'voucher_code': req.body.voucher_code,
-            'result': result
+            'quota': data.quota,
+            'total_used': data.total_used,
+            'total_left': data.total_left
           })
 
         })
         .catch((err) => {
           console.log('Error', err);
-          res.render('checkvoucher', { 'error': err });
+          res.render('checkvoucher', {
+            'error': err,
+            'captcha': recaptcha.render()
+          });
         })
     } else {
-      res.render('checkvoucher', { 'error': error })
+      res.render('checkvoucher', { 'error': error }, {
+        'captcha': recaptcha.render()
+      })
     }
   })
 
@@ -77,24 +104,35 @@ router.get('/api', function(req, res, next) {
 });
 
 router.post('/api', function(req, res, next) {
-  u.list_guests()
-    .then((data) => {
-      // remove the hyphen if any
-      var voucher = req.body.voucher.replace(/-/g, "");
-      // console.log(data.data.find(x => x.voucher_code === req.body.voucher));
-      // find a guest with voucher_code
-      var result = data.data.find(x => x.voucher_code === voucher);
-      res.json({
-        'response': result,
-        'voucher': voucher
-      });
+  if (!req.body.voucher_code) {
+    res.json({
+      'Error': 'No "voucher_code"',
+      'message': 'You did not send the voucher_code param'
     })
-    .then((data) => {
-      console.log('AP data', data);
-    })
-    .catch((err) => {
-      console.log('Error', err);
-    })
+  } else {
+    u.list_guests()
+      .then((data) => {
+        // remove the hyphen if any
+        var voucher = req.body.voucher_code.replace(/-/g, "");
+
+        var result = data.data.find(x => x.voucher_code === voucher);
+
+        var data = calculate_usage(result);
+
+        res.json({
+          'voucher_code': req.body.voucher_code,
+          'quota': data.quota,
+          'total_used': data.total_used,
+          'total_left': data.total_left
+        });
+      })
+      .catch((err) => {
+        console.log('Error', err);
+        res.json({
+          'Error': err
+        })
+      })
+  }
 })
 
 module.exports = router;
